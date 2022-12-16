8dea4287bb68ac1739513edfbaea9794a77a57d6
yang-xianzhu <2416548256@qq.com>
2022-12-17 00:09:53 +0800

    update

--- src/processor.ts
+++ src/processor.ts
@@ -3,7 +3,10 @@ const qs = require("fs");
 const path = require("path");
 
 // 区分：文件和文件夹函数
-function diffDirAndFile(dirFilesArr = [], basePath = "") {
+function diffDirAndFile(
+  dirFilesArr: Array<string> = [],
+  basePath: string = ""
+) {
   const result: IDiffDirAndFileResult = {
     dirs: [], // 存储目录文件
     files: [], // 存储普通文件
@@ -26,15 +29,15 @@ function diffDirAndFile(dirFilesArr = [], basePath = "") {
 // 读取文件函数
 function getSrcDir(options: IinstallOptions) {
   // 同步读取scr下的所有目录
-  const result = qs.readdirSync(path.resolve(__dirname, "./src"));
-  const diffResult = diffDirAndFile(result as any, "./src");
+  const result = qs.readdirSync(path.resolve(__dirname, "../src"));
+  const diffResult = diffDirAndFile(result as any, "../src");
 
   const resoleAliases: {
     [k in string]: string;
   } = {};
   diffResult.dirs.forEach((dirName) => {
     const key = `${options.keyName}${dirName}`;
-    const dirPath = path.resolve(__dirname, `./src/${dirName}`);
+    const dirPath = path.resolve(__dirname, `../src/${dirName}`);
     resoleAliases[key] = dirPath;
   });
   return resoleAliases;
